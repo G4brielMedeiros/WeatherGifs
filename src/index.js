187992e836992a5a7148fcfb9a56568e896a44e0
G4brielMedeiros <g4brielmedeiros@pm.me>
2022-04-07 16:30:55 -0300

    fix logic

--- src/index.js
+++ src/index.js
@@ -41,9 +41,9 @@ async function search() {
 
 searchButtonElement.addEventListener("click", search);
 
-getWeatherData("egypt", "metric").then((weather) => {
+getWeatherData("Egypt", "metric").then((weather) => {
     renderWeatherData(weather);
-    getGIFData(weather.place).then((data) => {
+    getGIFData(weather.title).then((data) => {
         renderGIFData(data);
     });
 });

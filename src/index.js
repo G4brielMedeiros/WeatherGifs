6379be28bf0e3e81e0c695ebc70befdc07403811
G4brielMedeiros <g4brielmedeiros@pm.me>
2022-04-07 11:10:04 -0300

    clean code

--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
 import { getWeatherData, logWeatherData } from "./openWeather";
 import { getElement, log } from "./utility";
 
-const searchElement = getElement("search-form");
-const searchInputElement = getElement("search-input");
-
-const weatherStatusElement = getElement("weather-status");
-
-searchElement.addEventListener("submit", () => {
-    logWeatherData(searchInputElement.value);
-});
-
 async function renderWeatherData(city) {
     const data = await getWeatherData(city);
 
     weatherStatusElement.innerText = data.weather[0].main;
+    log(data.weather[0].main);
 }
 
-logWeatherData("brasilia");
+const searchElement = getElement("search-form");
+const searchInputElement = getElement("search-input");
+const weatherStatusElement = getElement("weather-status");
 
-renderWeatherData("brasilia");
+searchElement.addEventListener("submit", () => {
+    renderWeatherData(searchInputElement.value);
+});

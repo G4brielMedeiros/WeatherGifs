a962488f3501c9ab8f9fae856e7ef83337a57932
G4brielMedeiros <g4brielmedeiros@pm.me>
2022-04-08 15:52:34 -0300

    remove bad constants

--- src/index.js
+++ src/index.js
@@ -2,57 +2,28 @@ import { getGIFData } from "./giphy";
 import { getCoords, getWeatherData, logWeatherData } from "./openWeather";
 import { getElement as $, log, normalize } from "./utility";
 
-const unitsButtonDOM = $("units");
-const searchFormDOM = $("search-form");
-const searchInputDOM = $("search-input");
-const languageButtonDOM = $("language");
-
-const cityDOM = $("city");
-const dateDOM = $("date");
-const temperatureTodayDOM = $("weather-temp");
-const weatherDescriptionDOM = $("weather-desc");
-const temperatureMinDOM = $("min");
-const temperatureMaxDOM = $("max");
-const temperatureFeelDOM = $("weather-feel");
-const gifDOM = $("gif");
-
-const weatherFuture1IconDOM = $("weather-future-icon-1");
-const weatherFuture1MinDOM = $("min-1");
-const weatherFuture1MaxDOM = $("max-1");
-const weatherFuture1Weekday = $("weather-future-1-weekday");
-
-const weatherFuture2IconDOM = $("weather-future-icon-3");
-const weatherFuture2MinDOM = $("min-2");
-const weatherFuture2MaxDOM = $("max-2");
-const weatherFuture2Weekday = $("weather-future-2-weekday");
-
-const weatherFuture3IconDOM = $("weather-future-icon-3");
-const weatherFuture3MinDOM = $("min-3");
-const weatherFuture3MaxDOM = $("max-3");
-const weatherFuture3Weekday = $("weather-future-3-weekday");
-
 function renderWeatherData(weather) {
     log(weather);
 
-    cityDOM.textContent = weather.place;
-    dateDOM.textContent = weather.date;
-    temperatureTodayDOM.textContent = weather.temp;
-    weatherDescriptionDOM.textContent = weather.description;
-    temperatureMinDOM.textContent = weather.min;
-    temperatureMaxDOM.textContent = weather.max;
-    temperatureFeelDOM.textContent = weather.feels;
+    $("city").textContent = weather.place;
+    $("date").textContent = weather.date;
+    $("weather-temp").textContent = weather.temp;
+    $("weather-desc").textContent = weather.description;
+    $("min").textContent = weather.min;
+    $("max").textContent = weather.max;
+    $("weather-feel").textContent = weather.feels;
 
-    weatherFuture1MinDOM.textContent = weather.future[0].min;
-    weatherFuture2MinDOM.textContent = weather.future[1].min;
-    weatherFuture3MinDOM.textContent = weather.future[2].min;
+    $("min-1").textContent = weather.future[0].min;
+    $("min-2").textContent = weather.future[1].min;
+    $("min-3").textContent = weather.future[2].min;
 
-    weatherFuture1MaxDOM.textContent = weather.future[0].max;
-    weatherFuture2MaxDOM.textContent = weather.future[1].max;
-    weatherFuture3MaxDOM.textContent = weather.future[2].max;
+    $("max-1").textContent = weather.future[0].max;
+    $("max-2").textContent = weather.future[1].max;
+    $("max-3").textContent = weather.future[2].max;
 
-    weatherFuture1Weekday.textContent = weather.future[0].dt;
-    weatherFuture2Weekday.textContent = weather.future[1].dt;
-    weatherFuture3Weekday.textContent = weather.future[2].dt;
+    $("weather-future-1-weekday").textContent = weather.future[0].dt;
+    $("weather-future-2-weekday").textContent = weather.future[1].dt;
+    $("weather-future-3-weekday").textContent = weather.future[2].dt;
 }
 
 // place
--- src/utility.js
+++ src/utility.js
@@ -2,4 +2,6 @@ export const log = (log) => console.log(log);
 
 export const getElement = (id) => document.getElementById(id);
 
+export const setText = (id, text) => getElement(id).textContent = text;
+
 export const normalize = (input) => input.toLowerCase().normalize("NFD").replace(/\p{Diacritic}/gu, "");

017eba03b78ac578a931f74c9cdea82321c32cef
G4brielMedeiros <g4brielmedeiros@pm.me>
2022-04-08 15:15:20 -0300

    remaster api call

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
 import { getGIFData } from "./giphy";
-import { getWeatherData, logWeatherData } from "./openWeather";
-import { getElement, log, normalize } from "./utility";
+import { getCoords, getWeatherData, logWeatherData } from "./openWeather";
+import { getElement as $, log, normalize } from "./utility";
 
-// const searchInputElement = getElement("search-input");
-// const searchInputMetricElement = getElement("metric");
-// const weatherStatusElement = getElement("weather-description");
-// const weatherImgElement = getElement("weather-gif");
-// const searchButtonElement = getElement("search-button");
-// const weatherTemperatureElement = getElement("weather-temperature");
+const unitsButtonDOM = $("units");
+const searchFormDOM = $("search-form");
+const searchInputDOM = $("search-input");
+const languageButtonDOM = $("language");
+const cityDOM = $("city");
+const dateDOM = $("date");
+const temperatureTodayDOM = $("weather-temp");
+const weatherDescriptionDOM = $("weather-desc");
+const temperatureMinDOM = $("min");
+const temperatureMaxDOM = $("max");
+const temperatureFeelDOM = $("weather-feel");
+const gifDOM = $("gif");
+
+const weatherFuture1IconDOM = $("weather-future-icon-1");
+const weatherFuture1MinMaxDOM = $("weather-future-1-min-max");
+const weatherFuture1Weekday = $("weather-future-1-weekday");
+
+const weatherFuture2IconDOM = $("weather-future-icon-3");
+const weatherFuture2MinMaxDOM = $("weather-future-3-min-max");
+const weatherFuture2Weekday = $("weather-future-3-weekday");
+
+const weatherFuture3IconDOM = $("weather-future-icon-3");
+const weatherFuture3MinMaxDOM = $("weather-future-3-min-max");
+const weatherFuture3Weekday = $("weather-future-3-weekday");
 
 function renderWeatherData(weather, units) {
     weatherStatusElement.innerText = weather.description;
@@ -35,13 +53,11 @@ function search() {
     weatherTemperatureElement.innerText = "loading...";
     weatherImgElement.src = "images/loading.gif";
 
-
-
     const units = searchInputMetricElement.checked ? "metric" : "imperial";
     const city = normalize(searchInputElement.value);
 
-    localStorage.setItem('city', city);
-    localStorage.setItem('units', units);
+    localStorage.setItem("city", city);
+    localStorage.setItem("units", units);
 
     log(units);
 
@@ -50,8 +66,13 @@ function search() {
 
 // searchButtonElement.addEventListener("click", search);
 
-let city = localStorage.getItem('city') || "brasilia";
-let units = localStorage.getItem('units') || "imperial";
+let city = localStorage.getItem("city") || "brasilia";
+let units = localStorage.getItem("units") || "imperial";
 
 //fetchAndRender(city, units);
 
+
+getCoords('recife').then(coords => {
+    log(coords)
+    getWeatherData(coords, 'metric').then(weather => log(weather))
+})
--- src/openWeather.js
+++ src/openWeather.js
@@ -1,27 +1,53 @@
 import { log } from "./utility";
 
-function getAPIcall(city, units) {
-    const API_KEY = "ca659f3e8873be553c03fadf808cffe3";
-    const API = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}&units=${units}`;
+const API_KEY = "ca659f3e8873be553c03fadf808cffe3";
+
+function getAPIcall(coords, units) {
+    const API = `https://api.openweathermap.org/data/2.5/onecall?lat=${coords.lat}&lon=${coords.lon}&appid=${API_KEY}&units=${units}&exclude=hourly,minutely`;
+    log(API)
     return API;
 }
 
-export async function getWeatherData(city, units = "standard") {
-    const response = await fetch(getAPIcall(city, units), { mode: "cors" });
+export async function getWeatherData(coords, units = "standard") {
+    const response = await fetch(getAPIcall(coords, units), { mode: "cors" });
 
     const data = await response.json();
 
-    log(data)
+    log(data);
     const formattedData = {
-        place: data.name,
-        temp: data.main.temp,
-        min: data.main.temp_min,
-        max: data.main.temp_max,
-        humidity: data.main.humidity,
-        description: data.weather[0].description,
-        title: data.weather[0].main,
+        place: coords.city,
+        temp: data.current.temp,
+        min: data.daily[0].temp.min,
+        max: data.daily[0].temp.max,
+        feels: data.current.feels_like,
+        description: data.current.weather[0].description,
+        future: [
+            {
+                dt: data.daily[1].dt,
+                min: data.daily[1].temp.min,
+                max: data.daily[1].temp.max,
+            },
+            {
+                dt: data.daily[2].dt,
+                min: data.daily[2].temp.min,
+                max: data.daily[2].temp.max,
+            },
+            {
+                dt: data.daily[3].dt,
+                min: data.daily[3].temp.min,
+                max: data.daily[3].temp.max,
+            },
+        ]
     };
     return formattedData;
+}
+
+export async function getCoords(city) {
+    const response = await fetch(
+        `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${API_KEY}`
+    );
+    const data = await response.json();
+    return {lat: data[0].lat, lon: data[0].lon, city: data[0].name};
 }
 
 export const kelvinToCelsius = (temp) => temp - 273.15;
